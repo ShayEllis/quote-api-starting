e7fc36285d0e5225b6d7dc27eedcc058825a7fb1
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-03-30 16:44:35 -0700

    finished express project

--- server.js
+++ server.js
@@ -8,3 +8,31 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`The server is running: http://localhost:${PORT}`)
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = {}
+    randomQuote.quote = getRandomElement(quotes)
+    res.send(randomQuote)
+})
+app.get('/api/quotes', (req, res, next) => {
+    const query = req.query.person
+    const selectedQuotes = {}
+    if (query) {
+        selectedQuotes.quotes = quotes.filter((quote) => quote.person === query)
+    } else {
+        selectedQuotes.quotes = quotes
+    }
+    res.send(selectedQuotes)
+})
+app.post('/api/quotes', (req, res, next) => {
+    const newQuote = {}
+    if (req.query.quote && req.query.person) {
+        newQuote.quote = req.query
+        res.send(newQuote)
+    } else {
+        res.status(400).send()
+    }
+})
